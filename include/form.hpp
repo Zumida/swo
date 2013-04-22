2de5333ddaf608173394eff012ffce986aa9e67c
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2013-04-23 01:41:17 +0900

    Add property methods interface.

--- include/form.hpp
+++ include/form.hpp
@@ -1,7 +1,7 @@
 /*
  * form.hpp
  *
- * Last modified: <2013/04/18 02:20:35 +0900> By Zumida
+ * Last modified: <2013/04/23 01:38:42 +0900> By Zumida
  */
 #ifndef FORM_HPP_INCLUDED
 #define FORM_HPP_INCLUDED
@@ -50,13 +50,19 @@ namespace swo {
 		void setRect(const WindowRect& rect);
 		void setMenu(const Menu& menu);
 
-		void setAcceptFiles(const bool enabled);
-		void setAppWindow(const bool enabled);
+		void setAcceptFiles(const bool enabled=true);
+		void setAppWindow(const bool enabled=true);
+		void setControlParent(const bool enabled=true);
 		void setFormType(const FormType formType);
-		void setMdiChild(const bool enabled);
-		void setActivate(const bool enabled);
-		void setTopMost(const bool enabled);
-		void setTransparent(const bool enabled);
+		void setMdiChild(const bool enabled=true);
+		void setNoActivate(const bool enabled=true);
+		void setTopMost(const bool enabled=true);
+		void setTransparent(const bool enabled=true);
+		
+		void setSwpFlags(const int swpFlags);
+
+		void setActiveWindow(void);
+		void setForegroundWindow(void);
 	};
 
 };
