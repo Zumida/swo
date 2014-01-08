82dfde59d68697c3f4dde4af28084ea82d0987bc
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-01-08 11:10:12 +0900

    Remove using namespace from hpp.

--- include/swo.hpp
+++ include/swo.hpp
@@ -1,14 +1,12 @@
 /*
  * swo.hpp
  *
- * Last modified: <2014/01/07 14:09:30 +0900> By Zumida
+ * Last modified: <2014/01/08 11:03:48 +0900> By Zumida
  */
 #ifndef SWO_HPP_INCLUDED
 #define SWO_HPP_INCLUDED
 
 #include "swoconfig.hpp"
 #include "application.hpp"
-
-using namespace swo;
 
 #endif
