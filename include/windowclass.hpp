f73b10dd0a11305259182b6cf233dc45953f03c8
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-01-30 20:00:12 +0900

    Update windowclass.
    Add inline namespace.
    Add local exception class.
    Update init sequence.
    Add throw exception.

--- include/windowclass.hpp
+++ include/windowclass.hpp
@@ -1,7 +1,7 @@
 /*
  * windowclass.hpp
  *
- * Last modified: <2014/01/30 16:41:28 +0900> By Zumida
+ * Last modified: <2014/01/30 19:54:00 +0900> By Zumida
  */
 #ifndef WINDOWCLASS_HPP_INCLUDED
 #define WINDOWCLASS_HPP_INCLUDED
@@ -11,38 +11,51 @@
 #include "cursor.hpp"
 #include "brush.hpp"
 #include "menu.hpp"
+#include "exception.hpp"
 
 namespace swo {
+	inline namespace window {
 
-	typedef LRESULT CALLBACK (*FuncWndProc)(HWND, UINT, WPARAM, LPARAM);
+		using FuncWndProc = LRESULT CALLBACK (*)(HWND, UINT, WPARAM, LPARAM);
 
-	class WindowClass : public Object {
-	protected:
-		String className;
-		String menuName;
-		Icon* icon;
-		Icon* smallIcon;
-		Cursor* cursor;
-		Brush* background;
-		FuncWndProc wndProc;
+		class WindowClass : public Object {
+		public:
+			class ExistsClassException : public swo::exception::Exception {
+			public:
+				ExistsClassException(const String& className);
+			};
 
-	public:
-		WindowClass();
-		~WindowClass();
+			class RegisterClassException : public swo::exception::Exception {
+			public:
+				RegisterClassException(const String& className);
+			};
 
-		WindowClass& setClassName(const String& className);
-		WindowClass& setIcon(const Icon& icon);
-		WindowClass& setSmallIcon(const Icon& smallIcon);
-		WindowClass& setCursor(const Cursor& cursor);
-		WindowClass& setBackground(const Brush& background);
-		WindowClass& setMenuName(const String& menuName);
-		WindowClass& setWndProc(const FuncWndProc wndProc);
+		protected:
+			String className;
+			String menuName;
+			Icon* icon;
+			Icon* smallIcon;
+			Cursor* cursor;
+			Brush* background;
+			FuncWndProc wndProc;
 
-		static void add(class WindowClass& wndClass);
-		static WindowClass* find(const String& className);
-		static WindowClass& create();
+		public:
+			WindowClass();
+			~WindowClass();
+
+			WindowClass& setClassName(const String& className);
+			WindowClass& setIcon(const Icon& icon);
+			WindowClass& setSmallIcon(const Icon& smallIcon);
+			WindowClass& setCursor(const Cursor& cursor);
+			WindowClass& setBackground(const Brush& background);
+			WindowClass& setMenuName(const String& menuName);
+			WindowClass& setWndProc(const FuncWndProc wndProc);
+
+			static void add(class WindowClass& wndClass);
+			static WindowClass* find(const String& className);
+			static WindowClass& create();
+		};
 	};
-
 };
 
 #endif
--- src/windowclass.cpp
+++ src/windowclass.cpp
@@ -1,7 +1,7 @@
 /*
  * windowclass.cpp
  *
- * Last modified: <2014/01/30 16:41:25 +0900> By Zumida
+ * Last modified: <2014/01/30 19:55:28 +0900> By Zumida
  */
 #include "swoconfig.hpp"
 #include "windowclass.hpp"
@@ -9,106 +9,121 @@
 #include "syscolorbrush.hpp"
 #include <map>
 
-using namespace swo;
+namespace swo {
+	inline namespace window {
 
-typedef std::map<String, class WindowClass*> WndClassMap;
+		typedef std::map<String, class WindowClass*> WndClassMap;
 
-static WndClassMap wndClassMap;
+		static WndClassMap wndClassMap;
 
-WindowClass::WindowClass() {
-	icon        = nullptr;
-	smallIcon   = nullptr;
-	cursor      = nullptr;
-	background  = nullptr;
-	wndProc     = nullptr;
-}
+		WindowClass::ExistsClassException::ExistsClassException(
+			const String& className)
+			: Exception(className) {}
 
-WindowClass::~WindowClass() {
-	if (!className.empty()) {
-		UnregisterClass(className.c_str(), ::GetModuleHandle(nullptr));
-	}
-}
+		WindowClass::RegisterClassException::RegisterClassException(
+			const String& className)
+			: Exception(className) {}
 
-WindowClass& WindowClass::setClassName(const String& className) {
-	this->className = className;
-	return *this;
-}
+		WindowClass::WindowClass()
+			: icon(nullptr), smallIcon(nullptr), cursor(nullptr),
+		background(nullptr), wndProc(nullptr) {}
 
-WindowClass& WindowClass::setIcon(const Icon& icon) {
-	this->icon = const_cast<Icon*>(&icon);
-	return *this;
-}
-
-WindowClass& WindowClass::setSmallIcon(const Icon& smallIcon) {
-	this->smallIcon = const_cast<Icon*>(&smallIcon);
-	return *this;
-}
-
-WindowClass& WindowClass::setCursor(const Cursor& cursor) {
-	this->cursor = const_cast<Cursor*>(&cursor);
-	return *this;
-}
-
-WindowClass& WindowClass::setBackground(const Brush& background) {
-	this->background = const_cast<Brush*>(&background);
-	return *this;
-}
-
-WindowClass& WindowClass::setWndProc(const FuncWndProc wndProc) {
-	this->wndProc = wndProc;
-	return *this;
-}
-
-void WindowClass::add(class WindowClass& wndClass) {
-	WNDCLASSEX wc = {0};
-
-	// ウィンドウクラスの情報を設定
-	wc.cbSize = sizeof(wc);                 // 構造体サイズ
-	wc.style = CS_HREDRAW | CS_VREDRAW;     // スタイル
-	wc.lpfnWndProc = wndClass.wndProc;      // ウィンドウプロシージャ
-	wc.cbClsExtra = 0;                      // 拡張情報１
-	wc.cbWndExtra = 0;                      // 拡張情報２
-	wc.hInstance = ::GetModuleHandle(nullptr); // インスタンスハンドル
-	wc.lpszClassName = wndClass.className.c_str(); // ウィンドウクラス名
-
-	// アイコン
-	if (wndClass.icon != nullptr)
-		wc.hIcon = wndClass.icon->getHandle();
-
-	// 小アイコン
-	if (wndClass.smallIcon != nullptr)
-		wc.hIconSm = wndClass.smallIcon->getHandle();
-	if (wc.hIconSm == nullptr) wc.hIconSm = wc.hIcon;
-
-	// マウスカーソル
-	if (wndClass.cursor != nullptr)
-		wc.hCursor = wndClass.cursor->getHandle();
-
-	// ウィンドウ背景
-	if (wndClass.background != nullptr) {
-		SysColorBrush* sb = dynamic_cast<SysColorBrush*>(wndClass.background);
-		if (sb != nullptr) {
-			wc.hbrBackground = (HBRUSH)(sb->getIndex() + 1);
-		} else {
-			wc.hbrBackground = wndClass.background->getHandle();
+		WindowClass::~WindowClass() {
+			if (!className.empty()) {
+				UnregisterClass(className.c_str(),
+								::GetModuleHandle(nullptr));
+			}
 		}
-	}
 
-	// メニュー名
-	if (!wndClass.menuName.empty())
-		wc.lpszMenuName  = wndClass.menuName.c_str();
+		WindowClass& WindowClass::setClassName(const String& className) {
+			this->className = className;
+			return *this;
+		}
 
-	// ウィンドウクラスを登録する
-	if (RegisterClassEx(&wc) != 0) {
-		wndClassMap.insert(std::make_pair(wndClass.className, &wndClass));
-	}
-}
+		WindowClass& WindowClass::setIcon(const Icon& icon) {
+			this->icon = const_cast<Icon*>(&icon);
+			return *this;
+		}
 
-WindowClass* WindowClass::find(const String& className) {
-	WndClassMap::iterator it = wndClassMap.find(className);
-	return (it == wndClassMap.end())? nullptr: it->second;
-}
+		WindowClass& WindowClass::setSmallIcon(const Icon& smallIcon) {
+			this->smallIcon = const_cast<Icon*>(&smallIcon);
+			return *this;
+		}
 
-WindowClass& WindowClass::create() {
-	return Instance::create<WindowClass>();
-}
+		WindowClass& WindowClass::setCursor(const Cursor& cursor) {
+			this->cursor = const_cast<Cursor*>(&cursor);
+			return *this;
+		}
+
+		WindowClass& WindowClass::setBackground(const Brush& background) {
+			this->background = const_cast<Brush*>(&background);
+			return *this;
+		}
+
+		WindowClass& WindowClass::setWndProc(const FuncWndProc wndProc) {
+			this->wndProc = wndProc;
+			return *this;
+		}
+
+		void WindowClass::add(class WindowClass& wndClass) {
+			// 同一クラス名の登録チェック
+			if (find(wndClass.className) != nullptr) {
+				throw ExistsClassException(wndClass.className);
+			}
+
+			WNDCLASSEX wc = {0};
+
+			// ウィンドウクラスの情報を設定
+			wc.cbSize = sizeof(wc);                 // 構造体サイズ
+			wc.style = CS_HREDRAW | CS_VREDRAW;     // スタイル
+			wc.lpfnWndProc = wndClass.wndProc;      // ウィンドウプロシージャ
+			wc.cbClsExtra = 0;                      // 拡張情報１
+			wc.cbWndExtra = 0;                      // 拡張情報２
+			wc.hInstance = ::GetModuleHandle(nullptr); // インスタンスハンドル
+			wc.lpszClassName = wndClass.className.c_str(); // ウィンドウクラス名
+
+			// アイコン
+			if (wndClass.icon != nullptr)
+				wc.hIcon = wndClass.icon->getHandle();
+
+			// 小アイコン
+			if (wndClass.smallIcon != nullptr)
+				wc.hIconSm = wndClass.smallIcon->getHandle();
+			if (wc.hIconSm == nullptr) wc.hIconSm = wc.hIcon;
+
+			// マウスカーソル
+			if (wndClass.cursor != nullptr)
+				wc.hCursor = wndClass.cursor->getHandle();
+
+			// ウィンドウ背景
+			if (wndClass.background != nullptr) {
+				SysColorBrush* sb = dynamic_cast<SysColorBrush*>(wndClass.background);
+				if (sb != nullptr) {
+					wc.hbrBackground = (HBRUSH)(sb->getIndex() + 1);
+				} else {
+					wc.hbrBackground = wndClass.background->getHandle();
+				}
+			}
+
+			// メニュー名
+			if (!wndClass.menuName.empty())
+				wc.lpszMenuName  = wndClass.menuName.c_str();
+
+			// ウィンドウクラスを登録する
+			ATOM atom = ::RegisterClassEx(&wc);
+			if (atom == 0) {
+				throw RegisterClassException(wndClass.className);
+			}
+			wndClassMap.insert(std::make_pair(wndClass.className, &wndClass));
+		}
+
+		WindowClass* WindowClass::find(const String& className) {
+			WndClassMap::iterator it = wndClassMap.find(className);
+			return (it == wndClassMap.end())? nullptr: it->second;
+		}
+
+		WindowClass& WindowClass::create() {
+			return Instance::create<WindowClass>();
+		}
+	};
+};
