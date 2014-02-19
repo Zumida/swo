cbe02da06f268c1b53873f299e4d8276236c35a3
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-02-19 21:55:34 +0900

    Add inline namespace.

--- include/syscursor.hpp
+++ include/syscursor.hpp
@@ -1,7 +1,7 @@
 /*
  * syscursor.hpp
  *
- * Last modified: <2013/05/22 06:49:49 +0900> By Zumida
+ * Last modified: <2014/02/17 22:00:13 +0900> By Zumida
  */
 #ifndef SYSCURSOR_HPP_INCLUDED
 #define SYSCURSOR_HPP_INCLUDED
@@ -9,28 +9,30 @@
 #include "cursor.hpp"
 
 namespace swo {
+	inline namespace image {
 
-	class SysCursor : public Cursor {
-	public:
-		SysCursor(const TCHAR* idc);
-		~SysCursor();
+		class SysCursor : public Cursor {
+		private:
+			SysCursor(const TCHAR* idc);
+			~SysCursor();
 
-		const static class SysCursor AppStarting;
-		const static class SysCursor Arrow;
-		const static class SysCursor Cross;
-		const static class SysCursor Hand;
-		const static class SysCursor Help;
-		const static class SysCursor IBeam;
-		const static class SysCursor No;
-		const static class SysCursor SizeAll;
-		const static class SysCursor SizeNESW;
-		const static class SysCursor SizeNS;
-		const static class SysCursor SizeNWSE;
-		const static class SysCursor SizeWE;
-		const static class SysCursor Uparrow;
-		const static class SysCursor Wait;
+		public:
+			const static class SysCursor AppStarting;
+			const static class SysCursor Arrow;
+			const static class SysCursor Cross;
+			const static class SysCursor Hand;
+			const static class SysCursor Help;
+			const static class SysCursor IBeam;
+			const static class SysCursor No;
+			const static class SysCursor SizeAll;
+			const static class SysCursor SizeNESW;
+			const static class SysCursor SizeNS;
+			const static class SysCursor SizeNWSE;
+			const static class SysCursor SizeWE;
+			const static class SysCursor Uparrow;
+			const static class SysCursor Wait;
+		};
 	};
-
 };
 
 #endif
--- src/syscursor.cpp
+++ src/syscursor.cpp
@@ -1,31 +1,34 @@
 /*
  * syscursor.cpp
  *
- * Last modified: <2014/01/07 14:29:00 +0900> By Zumida
+ * Last modified: <2014/02/17 23:15:53 +0900> By Zumida
  */
 #include "swoconfig.hpp"
 #include "syscursor.hpp"
 
-using namespace swo;
+namespace swo {
+	inline namespace image {
 
-SysCursor::SysCursor(const TCHAR* idc) {
-	Image::load(idc, IMAGE_CURSOR);
-}
+		SysCursor::SysCursor(const TCHAR* idc) {
+			Image::load(nullptr, idc, IMAGE_CURSOR);
+		}
 
-SysCursor::~SysCursor() {
-}
+		SysCursor::~SysCursor() {
+		}
 
-const SysCursor SysCursor::AppStarting(IDC_APPSTARTING);
-const SysCursor SysCursor::Arrow(IDC_ARROW);
-const SysCursor SysCursor::Cross(IDC_CROSS);
-const SysCursor SysCursor::Hand(IDC_HAND);
-const SysCursor SysCursor::Help(IDC_HELP);
-const SysCursor SysCursor::IBeam(IDC_IBEAM);
-const SysCursor SysCursor::No(IDC_NO);
-const SysCursor SysCursor::SizeAll(IDC_SIZEALL);
-const SysCursor SysCursor::SizeNESW(IDC_SIZENESW);
-const SysCursor SysCursor::SizeNS(IDC_SIZENS);
-const SysCursor SysCursor::SizeNWSE(IDC_SIZENWSE);
-const SysCursor SysCursor::SizeWE(IDC_SIZEWE);
-const SysCursor SysCursor::Uparrow(IDC_UPARROW);
-const SysCursor SysCursor::Wait(IDC_WAIT);
+		const SysCursor SysCursor::AppStarting(IDC_APPSTARTING);
+		const SysCursor SysCursor::Arrow(IDC_ARROW);
+		const SysCursor SysCursor::Cross(IDC_CROSS);
+		const SysCursor SysCursor::Hand(IDC_HAND);
+		const SysCursor SysCursor::Help(IDC_HELP);
+		const SysCursor SysCursor::IBeam(IDC_IBEAM);
+		const SysCursor SysCursor::No(IDC_NO);
+		const SysCursor SysCursor::SizeAll(IDC_SIZEALL);
+		const SysCursor SysCursor::SizeNESW(IDC_SIZENESW);
+		const SysCursor SysCursor::SizeNS(IDC_SIZENS);
+		const SysCursor SysCursor::SizeNWSE(IDC_SIZENWSE);
+		const SysCursor SysCursor::SizeWE(IDC_SIZEWE);
+		const SysCursor SysCursor::Uparrow(IDC_UPARROW);
+		const SysCursor SysCursor::Wait(IDC_WAIT);
+	};
+};
